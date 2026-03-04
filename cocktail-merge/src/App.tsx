f0fbbfd0cf3aad2f5c7c3b73d785edc9944fef4f
liuyaran1-rgb <liuyaran1@gmail.com>
2026-03-04 11:49:25 +0900

    Delete cocktail-merge directory

--- cocktail-merge/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import CocktailGame from './components/CocktailGame';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-[#1a1a1a]">
-      <CocktailGame />
-    </div>
-  );
-}
